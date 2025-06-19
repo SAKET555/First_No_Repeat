a5147f5592b9b5e757aadc5f0e23f85b5671b16c
SAKET-HEGDE <sakethegde555@gmail.com>
2025-06-19 10:43:24 +0530

    My first commit

---
+++ My_No_Vowel_Word_Detector.py
@@ -0,0 +1,15 @@
+def Check_word(input_string):
+    List = input_string.split()
+    longest_word = ""
+    vowels = set("aeiouAEIOU")
+    for word in List:
+        if all(char not in vowels for char in word):
+            if len(longest_word) < len(word):
+                longest_word = word
+    if longest_word ==  "":
+        return "No word without vowels"
+    return longest_word
+
+input_string = input("Sentence : ")
+answer = Check_word(input_string)
+print("Answer is ", answer)
